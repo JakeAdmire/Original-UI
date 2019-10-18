6314375cfe8beaf1793d36a71cf4ad9ac28d9d2d
Jake <jakeadmire1@gmail.com>
2019-10-18 17:15:34 -0600

    Add smoke tests for existing components

---
+++ src/__tests__/icon.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable padded-blocks */
+import { Icon } from '../components/Icon/Icon'
+
+describe('Icon', () => {
+
+  it('is truthy', () => {
+    expect(Icon).toBeTruthy()
+  })
+
+})
