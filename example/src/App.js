99dfc8ceb53d398b33ebf606a3dcb23458949ca6
Jake <jakeadmire1@gmail.com>
2019-10-20 17:39:56 -0600

    Update example TextInput & Icon render

--- example/src/App.js
+++ example/src/App.js
@@ -3,13 +3,33 @@ import React, { Component } from 'react'
 import { TextInput, Icon } from 'original-ui'
 
 export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isTextHidden: true
+    }
+  }
+
+  toggleHiddenText = () => {
+    this.setState({
+      isTextHidden: !this.state.isTextHidden
+    })
+  }
+
   render () {
     return (
       <div>
         <TextInput 
-          placeholder="ex. 'thoughtfulpassword123...'" 
+          placeholderText='ex. "thoughtfulpassword123..."'
           labelText='Enter your password:' 
-          rightIcon={<Icon name='eye-slash' />}
+          isPassInput={this.state.isTextHidden}
+          rightIcon={
+            <Icon 
+              iconName={this.state.isTextHidden ? 'eye' : 'eye-slash'}
+              iconSize='24px'
+              onPress={this.toggleHiddenText}
+            />
+          }
         />
       </div>
     )
