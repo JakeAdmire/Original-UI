f7c970fc037a6f1b8991a564b81a1a718b95e908
Jake <jakeadmire1@gmail.com>
2019-10-22 20:59:06 -0600

    Test tooManyParameters function

--- src/__tests__/helpers.test.js
+++ src/__tests__/helpers.test.js
@@ -14,3 +14,17 @@ describe('isRequired', () => {
   })
 
 })
+
+describe('tooManyParameters', () => {
+
+  it('should throw an error when called', () => {
+    let param1 = 'Icon'
+    let param2 = ['isFlippedVertically', 'isFlippedHorizontally']
+    let errorMessage = `'${param1}' cannot take more than one of the following parameters: ${param2}`
+
+    expect(() => {
+      tooManyParameters(param1, param2)
+    }).toThrow(errorMessage)
+  })
+
+})
