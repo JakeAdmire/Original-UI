13ee2cb1a8842df1d5afee2162bfe8b97fcadcf5
Jake <jakeadmire1@gmail.com>
2019-10-24 19:42:20 -0600

    Refactor component into functional container

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,92 +1,91 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 
-export class TextInput extends Component {
-  static propTypes = {
-    placeholderText: PropTypes.string,
-    labelText: PropTypes.string,
-    labelStyle: PropTypes.string,
-    width: PropTypes.string,
-    height: PropTypes.string,
-    textStyle: PropTypes.string,
-    onChange: PropTypes.func,
-    isPassInput: PropTypes.bool,
-    leftIcon: PropTypes.object,
-    rightIcon: PropTypes.object,
-    value: PropTypes.string
-  };
+export function TextInput({
+  placeholderText = '',
+  labelText,
+  labelStyle = { color: 'orange' },
+  width = '100%',
+  height = '50px',
+  textStyle = {
+    fontSize: '16px',
+    color: 'grey'
+  },
+  onChange = null,
+  isPassInput = false,
+  leftIcon,
+  rightIcon,
+  value
+}) {
+  const [isShadowShown, updateShadow] = useState(false)
 
-  constructor(props) {
-    super(props)
-    this.state = { isShadowShown: false }
-  }
+  // ERROR HANDLERS HERE ---
 
-  updateShadow = () => {
-    this.setState({ isShadowShown: !this.state.isShadowShown })
-  }
+  // ERROR HANDLERS HERE ^^^
 
-  render() {
-    const {
-      placeholderText = '',
-      labelText,
-      labelStyle = { color: 'orange' },
-      width = '100%',
-      height = '50px',
-      textStyle = {
-        fontSize: '16px',
-        color: 'grey'
-      },
-      onChange = null,
-      isPassInput = false,
-      leftIcon,
-      rightIcon,
-      value
-    } = this.props
-
-    let divInlineStyles = {
+  const divInlineStyles = () => {
+    return {
       width: width,
       height: height
     }
-
-    let wrapperClass = `
-      ${styles.inputwrapper}
-      ${this.state.isShadowShown && styles.inputshadow}
-    `
-
-    return (
-      <div>
-        {
-          labelText && labelText.length &&
-            <label
-              htmlFor={styles.textinput}
-              className={styles.inputlabel}
-              style={labelStyle}>
-              {labelText}
-            </label>
-        }
-        <div
-          className={wrapperClass}
-          style={divInlineStyles}>
-          {
-            leftIcon && leftIcon.props && leftIcon
-          }
-          <input
-            type={isPassInput ? 'password' : 'text'}
-            value={value}
-            id={styles.textinput}
-            className={styles.textinput}
-            placeholder={placeholderText}
-            style={textStyle}
-            onChange={(e) => onChange !== null ? (e) => onChange(e) : null}
-            onFocus={this.updateShadow}
-            onBlur={this.updateShadow}/>
-          {
-            rightIcon && rightIcon.props && rightIcon
-          }
-        </div>
-      </div>
-    )
   }
+
+  const wrapperClass = () => {
+    let className = ''
+    className += styles.inputwrapper
+    if (isShadowShown) className += ' ' + styles.inputshadow
+    return className
+  }
+
+  return (
+    <div>
+      {
+        labelText && labelText.length &&
+          <label
+            htmlFor={styles.textinput}
+            className={styles.inputlabel}
+            style={labelStyle}
+          >
+            {labelText}
+          </label>
+      }
+      <div
+        className={wrapperClass()}
+        style={divInlineStyles()}>
+        {
+          leftIcon && leftIcon.props && leftIcon
+        }
+        <input
+          type={isPassInput ? 'password' : 'text'}
+          value={value}
+          id={styles.textinput}
+          className={styles.textinput}
+          placeholder={placeholderText}
+          style={textStyle}
+          onChange={(e) => onChange !== null ? onChange(e) : null}
+          onFocus={() => updateShadow(!isShadowShown)}
+          onBlur={() => updateShadow(!isShadowShown)}
+        />
+        {
+          rightIcon && rightIcon.props && rightIcon
+        }
+      </div>
+    </div>
+  )
+}
+
+TextInput.propTypes = {
+  placeholderText: PropTypes.string,
+  labelText: PropTypes.string,
+  labelStyle: PropTypes.string,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  textStyle: PropTypes.string,
+  onChange: PropTypes.func,
+  isPassInput: PropTypes.bool,
+  leftIcon: PropTypes.object,
+  rightIcon: PropTypes.object,
+  value: PropTypes.string
 }
